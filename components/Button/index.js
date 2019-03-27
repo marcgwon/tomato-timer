0252cc437b13e28bd9390430fe9a4c14371220d1
Marc Gwon <marc@Marc-MacBook-Pro.local>
2019-03-27 17:25:33 +0900

    init

---
+++ components/Button/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  View,
+  TouchableOpacity,
+  TouchableHighlight,
+  StyleSheet,
+} from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+
+function Button({ iconName, onPress }) {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <FontAwesome name={iconName} size={80} color={'white'} />
+    </TouchableOpacity>
+  );
+}
+
+export default Button;
